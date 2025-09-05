8cf8487974b9e746dcaf0f5f43b3019e96f99f81
codingmatemoon <janghyun2478@gmail.com>
2025-09-05 15:34:58 +0900

    자바 코드 구성 정보 사용

--- src/main/java/cmmoon/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/cmmoon/tobyspringboot/TobySpringBootApplication.java
@@ -12,20 +12,40 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
+/* Bean 오브젝트 팩토리 메서드를 가진 클래스임을 명시. AnnotationConfig를 이용하는 ApplicationContext에 처음으로 등록됨(@Configuration 붙은 클래스)
+ */
+@Configuration
 public class TobySpringBootApplication {
 
+    @Bean
+    public HelloController helloController(HelloService helloService){
+        return new HelloController(helloService);
+    }
+
+    // 스프링 컨테이너의 Bean 오브젝트를 만드는 팩토리 메서드임을 명시
+    @Bean
+    public HelloService helloService(){
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+        /*
+        자바 코드로 만든 Configuration 정보를 읽기 위한 클래스로 변경
+         */
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -36,9 +56,7 @@ public class TobySpringBootApplication {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        // container에 HelloService 인터페이스를 구현한 bean 찾아서 의존성 주입
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(TobySpringBootApplication.class);
         // bean 오브젝트 생성
         applicationContext.refresh();
 
