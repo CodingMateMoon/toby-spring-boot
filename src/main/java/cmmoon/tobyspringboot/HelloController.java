2581027e3eca093349e28403deb225cc6cb08320
codingmatemoon <janghyun2478@gmail.com>
2025-09-11 13:43:19 +0900

    Bean의 생명주기 메서드. 생성자를 통해 ApplicationContext 주입 받는 방식 활용

--- src/main/java/cmmoon/tobyspringboot/HelloController.java
+++ src/main/java/cmmoon/tobyspringboot/HelloController.java
@@ -1,8 +1,6 @@
 package cmmoon.tobyspringboot;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +11,16 @@ import java.util.Objects;
 @Component
 메타 애노테이션 : 애노테이션 위에 붙은 애노테이션
  */
-public class HelloController implements ApplicationContextAware {
+public class HelloController {
     private final HelloService helloService;
     // 생성자를 통해 인스턴스가 만들어진 이후에 setApplicationContext 호출하기 때문에 final은 불가
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
@@ -28,10 +29,12 @@ public class HelloController implements ApplicationContextAware {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 
+    /*
     //Root WebApplicationContext, started on Tue Sep 09 08:55:05 KST 2025
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println(applicationContext);
         this.applicationContext = applicationContext;
     }
+     */
 }
