3222127f04e440193ceef85ea19096d172e6d2f7
codingmatemoon <janghyun2478@gmail.com>
2025-09-23 16:15:23 +0900

    chore: 불필요한 주석 제거

--- src/test/java/cmmoon/tobyspringboot/HelloApiTest.java
+++ src/test/java/cmmoon/tobyspringboot/HelloApiTest.java
@@ -29,15 +29,10 @@ public class HelloApiTest {
     }
     @Test
     void failsHelloApi() {
-        // HTTPie
-        // http localhost:8080/hello?name=Spring
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> res
                 = rest.getForEntity("http://localhost:8080/hello?name", String.class);
-
-        // 응답 검증
-        // status code 200
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
