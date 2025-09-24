6cffdcd07ca2e0b8cfb1aaad83610a3241bc3d5d
codingmatemoon <janghyun2478@gmail.com>
2025-09-24 11:36:41 +0900

    HelloService를 구현한 Bean이 여러 개 일 경우 오류 발생

--- src/main/java/cmmoon/tobyspringboot/HelloDecorator.java
+++ src/main/java/cmmoon/tobyspringboot/HelloDecorator.java
@@ -1,9 +1,12 @@
 package cmmoon.tobyspringboot;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 // Caused by: org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'cmmoon.tobyspringboot.HelloService' available: expected single matching bean but found 2: helloDecorator,simpleHelloService
 @Service
+// HelloController가 필요한 빈이 2개일 때 @Primary 붙은 빈을 우선적으로 주입
+@Primary
 public class HelloDecorator implements HelloService{
     private final HelloService helloService;
 
