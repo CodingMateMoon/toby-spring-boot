8b3fb26407e1d3ad30f2650d63d9aef57b567346
codingmatemoon <janghyun2478@gmail.com>
2025-09-30 03:15:27 +0900

    합성 애노테이션 적용

---
+++ src/main/java/cmmoon/tobyspringboot/MySpringBootAnnotation.java
@@ -0,0 +1,18 @@
+package cmmoon.tobyspringboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// 런타임까지 애노테이션 정보가 유지되도록 설정
+@Retention(RetentionPolicy.RUNTIME)
+// 클래스, 인터페이스, enum 등에 적용할 때 사용
+@Target(ElementType.TYPE)
+@Configuration
+@ComponentScan
+public @interface MySpringBootAnnotation {
+}
--- src/main/java/cmmoon/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/cmmoon/tobyspringboot/TobySpringBootApplication.java
@@ -3,15 +3,16 @@ package cmmoon.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@MySpringBootAnnotation
 /* Bean 오브젝트 팩토리 메서드를 가진 클래스임을 명시. AnnotationConfig를 이용하는 ApplicationContext에 처음으로 등록됨(@Configuration 붙은 클래스)
  */
+/*
 @Configuration
 //해당 클래스가 속한 패키지부터 시작해서 하위 패키지를 전부 탐색해서 컴포넌트가 붙은 클래스들을 전부 빈으로 등록
 @ComponentScan
+*/
 //@SpringBootApplication
 public class TobySpringBootApplication {
     // ServletWebServerFactory, DispatcherServlet 빈으로 등록할 경우 유연한 구성 정보 설정 가능
@@ -27,6 +28,7 @@ public class TobySpringBootApplication {
 
     public static void main(String[] args) {
         // Configuration, ComponantScan 팩토리 메서드를 가지고 스프링 컨테이너에게 애플리케이션 구성을 어떻게 할 것인가에 대한 정보를 가진 클래스
+        // curl -v "http://localhost:8080/hello?name=test"
         MySpringApplication.run(TobySpringBootApplication.class, args);
 //        SpringApplication.run(TobySpringBootApplication.class, args);
     }
