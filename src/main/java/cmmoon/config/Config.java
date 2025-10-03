487a4133873c1aade149c38ee9321d9bcc37c34c
codingmatemoon <janghyun2478@gmail.com>
2025-10-03 09:29:06 +0900

    Config 클래스 별도 패키지로 분리

--- src/main/java/cmmoon/config/Config.java
+++ src/main/java/cmmoon/config/Config.java
@@ -1,4 +1,4 @@
-package cmmoon.tobyspringboot;
+package cmmoon.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
+/*
+컴포넌트 스캔에 대한 범위를 명시하지 않을 경우 같은 패키지 또는 하위 패키지의 클래스들을 대상으로 스캔하는게 기본 설정이기 때문에
+다른 패키지에 존재하는 컴포넌트는 스캔 대상에서 제외됨.
+ */
 @Configuration
 public class Config {
     // ServletWebServerFactory, DispatcherServlet 빈으로 등록할 경우 유연한 구성 정보 설정 가능
