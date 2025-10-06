335090c569bcc4635f8629efca14737ec627567d
codingmatemoon <janghyun2478@gmail.com>
2025-10-06 18:23:49 +0900

    여러 애노테이션들을 import하는 메타 애노테이션 (EnableMyAutoConfiguration)

--- src/main/java/cmmoon/tobyspringboot/MySpringBootApplication.java
+++ src/main/java/cmmoon/tobyspringboot/MySpringBootApplication.java
@@ -1,10 +1,8 @@
 package cmmoon.tobyspringboot;
 
-import cmmoon.config.autoconfig.DispatcherServletConfig;
-import cmmoon.config.autoconfig.TomcatWebServerConfig;
+import cmmoon.config.autoconfig.EnableMyAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -17,6 +15,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+@EnableMyAutoConfiguration
 public @interface MySpringBootApplication {
 }
