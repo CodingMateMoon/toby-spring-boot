828d651947a0b9ff3d6311b873a050db755fe768
codingmatemoon <janghyun2478@gmail.com>
2025-10-12 21:07:31 +0900

    ClassLoader, ImportCandidates 활용

--- src/main/java/cmmoon/config/MyAutoConfigImportSelector.java
+++ src/main/java/cmmoon/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,23 @@
 package cmmoon.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    // 자동 구성 설정 후보
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "cmmoon.config.autoconfig.DispatcherServletConfig",
-                "cmmoon.config.autoconfig.TomcatWebServerConfig"
-        };
+//        ImportCandidates candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
