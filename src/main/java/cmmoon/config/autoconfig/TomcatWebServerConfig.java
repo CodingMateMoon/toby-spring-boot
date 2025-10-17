f3eed6e71606b99fab3d8471d0919aa7b5ed3f78
codingmatemoon <janghyun2478@gmail.com>
2025-10-17 14:56:59 +0900

    ImportSelector 활용 ImportCandiates.load에서 사용할 패키지명.annotation-name.imports 파일 생성

--- src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
@@ -3,7 +3,9 @@ package cmmoon.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class TomcatWebServerConfig {
     // ServletWebServerFactory, DispatcherServlet 빈으로 등록할 경우 유연한 구성 정보 설정 가능
     @Bean
