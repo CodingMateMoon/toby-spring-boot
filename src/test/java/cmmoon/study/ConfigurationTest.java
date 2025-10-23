56780cb9c6826a9054b986f119818bbd6bdb0af7
codingmatemoon <janghyun2478@gmail.com>
2025-10-23 20:24:33 +0900

    @Configuration과 proxyBeanMethods. Bean1, Bean2에서 사용하는 의존 객체 동일하게 사용

--- src/test/java/cmmoon/study/ConfigurationTest.java
+++ src/test/java/cmmoon/study/ConfigurationTest.java
@@ -3,15 +3,24 @@ package cmmoon.study;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 public class ConfigurationTest {
     @Test
     void configuration() {
-        MyConfig myConfig = new MyConfig();
+/*
+        MyConfig myConfig = new MyConfig);
         Bean1 bean1 = myConfig.bean1();
         Bean2 bean2 = myConfig.bean2();
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+*/
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2 = ac.getBean(Bean2.class);
         Assertions.assertThat(bean1.common).isSameAs(bean2.common);
     }
 
