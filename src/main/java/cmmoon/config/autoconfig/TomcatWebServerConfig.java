0814dbbf80704798d2fa26eed1ae2f821325ecf5
codingmatemoon <janghyun2478@gmail.com>
2025-10-28 00:04:20 +0900

    JettyWebServerConfig 추가

--- src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
     // ServletWebServerFactory, DispatcherServlet 빈으로 등록할 경우 유연한 구성 정보 설정 가능
-    @Bean
+    @Bean("TomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
