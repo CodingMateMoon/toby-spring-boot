2fca40fd2d607266ba0f1038e9a2b4453b360a9f
codingmatemoon <janghyun2478@gmail.com>
2025-11-10 13:32:25 +0900

    Conditional 테스트

--- src/test/java/cmmoon/study/ConditionalTest.java
+++ src/test/java/cmmoon/study/ConditionalTest.java
@@ -1,21 +1,44 @@
 package cmmoon.study;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConditionalTest {
     @Test
     void conditional(){
         // true
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(MyBean.class);
+                    assertThat(context).hasSingleBean(Config1.class);
+                });
+
+       new ApplicationContextRunner().withUserConfiguration(Config2.class)
+                .run(context -> {
+                    assertThat(context).doesNotHaveBean(MyBean.class);
+                    assertThat(context).doesNotHaveBean(Config1.class);
+                });
+
+/*
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
         ac.register(Config1.class);
         ac.refresh();
 
         MyBean bean = ac.getBean(MyBean.class);
 
-
         //false
+        AnnotationConfigApplicationContext ac2 = new AnnotationConfigApplicationContext();
+        ac.register(Config1.class);
+        ac.refresh();
+
+        MyBean bean2 = ac2.getBean(MyBean.class);
+*/
+
     }
 
     @Configuration
