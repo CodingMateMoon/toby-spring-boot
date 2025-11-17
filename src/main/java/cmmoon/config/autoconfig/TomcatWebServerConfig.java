ef494fe293ef9378f532498add69e4ecf1a87edd
codingmatemoon <janghyun2478@gmail.com>
2025-11-17 16:35:21 +0900

    @ConditionalMyOnClass 애노테이션을 적용해서 jetty, tomcat 등 이름을 value로 전달하여 서버 설정 구성

--- src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
@@ -1,17 +1,14 @@
 package cmmoon.config.autoconfig;
 
+import cmmoon.config.ConditionalMyOnClass;
 import cmmoon.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+//@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
     // ServletWebServerFactory, DispatcherServlet 빈으로 등록할 경우 유연한 구성 정보 설정 가능
     @Bean("tomcatWebServerFactory")
@@ -20,10 +17,12 @@ public class TomcatWebServerConfig {
     }
 
 
+/*
     static class TomcatCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
             return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
         }
     }
+*/
 }
