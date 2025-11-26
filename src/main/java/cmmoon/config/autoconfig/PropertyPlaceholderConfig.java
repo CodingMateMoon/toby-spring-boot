072433ed1610245a04da9f5dd6424fe99ec27674
codingmatemoon <janghyun2478@gmail.com>
2025-11-26 12:11:20 +0900

    @Value, PropertySourcesPlaceholderConfigurer 활용 contextPath 설정

---
+++ src/main/java/cmmoon/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,14 @@
+package cmmoon.config.autoconfig;
+
+import cmmoon.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/cmmoon/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package cmmoon.config.autoconfig;
 
 import cmmoon.config.ConditionalMyOnClass;
 import cmmoon.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -12,6 +13,10 @@ import org.springframework.core.env.Environment;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 //@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
+
+    @Value("${contextPath}")
+    String contextPath;
+
     // ServletWebServerFactory, DispatcherServlet 빈으로 등록할 경우 유연한 구성 정보 설정 가능
     @Bean("tomcatWebServerFactory")
     /*
@@ -23,7 +28,9 @@ public class TomcatWebServerConfig {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         // 모든 서블릿 매핑 앞에 해당 path가 붙음
         // curl -v "http://localhost:8080/app/hello?name=test"
-        factory.setContextPath(env.getProperty("contextPath"));
+        System.out.println("contextPath : " + this.contextPath);
+        factory.setContextPath(this.contextPath);
+//        factory.setContextPath(env.getProperty("contextPath"));
 //        factory.setContextPath("/app");
         return factory;
     }
