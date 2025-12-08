79590f4973332f731b9bb74edfc29684aa400b37
codingmatemoon <janghyun2478@gmail.com>
2025-12-08 12:33:19 +0900

    DataSource 자동 구성 클래스

---
+++ src/main/java/cmmoon/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package cmmoon.config.autoconfig;
+
+import cmmoon.config.ConditionalMyOnClass;
+import cmmoon.config.EnableMyConfigurationProperties;
+import cmmoon.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+    @Bean DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        return dataSource;
+    }
+}
