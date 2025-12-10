b9045c7b1f70d4a346404aa4ffb88570377f702d
codingmatemoon <janghyun2478@gmail.com>
2025-12-11 00:57:34 +0900

    DataSource 자동 구성 클래스) JdbcTemplate 설정 추가

---
+++ src/test/java/cmmoon/tobyspringboot/DataSouceTest.java
@@ -0,0 +1,20 @@
+package cmmoon.tobyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@HelloBootTest
+public class DataSouceTest {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void connect() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        connection.close();
+    }
+}
