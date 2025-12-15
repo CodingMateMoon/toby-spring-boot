93abbded230ef094ab9e518a8cd332a8c6a05cf3
codingmatemoon <janghyun2478@gmail.com>
2025-12-15 23:33:17 +0900

    DataSource 자동 구성 클래스) JdbcTemplateTest. @BeforeEach 활용 테스트 전 매번 초기화 수행

--- src/test/java/cmmoon/tobyspringboot/JdbcTemplateTest.java
+++ src/test/java/cmmoon/tobyspringboot/JdbcTemplateTest.java
@@ -25,4 +25,12 @@ public class JdbcTemplateTest {
         Assertions.assertThat(count).isEqualTo(2);
     }
 
+    @Test
+    void insertAndQuery2() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "Toby", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
 }
