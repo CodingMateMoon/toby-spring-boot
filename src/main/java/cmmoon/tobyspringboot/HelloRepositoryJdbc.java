0b6c77f7cf1b417288955ec82d110a843b1ad27e
codingmatemoon <janghyun2478@gmail.com>
2025-12-25 18:59:37 +0900

    Hello 리포지토리) HelloRepository null 체크 테스트

--- src/main/java/cmmoon/tobyspringboot/HelloRepositoryJdbc.java
+++ src/main/java/cmmoon/tobyspringboot/HelloRepositoryJdbc.java
@@ -1,11 +1,8 @@
 package cmmoon.tobyspringboot;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @Repository
 public class HelloRepositoryJdbc implements HelloRepository {
@@ -17,18 +14,21 @@ public class HelloRepositoryJdbc implements HelloRepository {
 
     @Override
     public Hello findHello(String name) {
-        return jdbcTemplate.queryForObject("select * from hello where name = '" + name + "'",
-                new RowMapper<Hello>() {
-
-                    @Override
-                    public Hello mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        return null;
-                    }
-                });
+        try {
+            return jdbcTemplate.queryForObject("select * from hello where name = '" + name + "'",
+                    (rs, rowNum) -> new Hello(
+                            rs.getString("name"),
+                            rs.getInt("count")
+                    ));
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public void increaseCount(String name) {
-
+        Hello hello = findHello(name);
+        if (hello == null) jdbcTemplate.update("insert into hello values(?,?)", name, 1);
+        else jdbcTemplate.update("update hello set count = ? where name = ?", hello.getCount() + 1, name);
     }
 }
