7db1e51278b5705e17abf6f6a754e79605214d28
codingmatemoon <janghyun2478@gmail.com>
2026-01-09 10:59:55 +0900

    스프링 부트의 자동 구성과 테스트로 전환 / DB 관련 테스트 자동 롤백 처리 위해 @Transactional 추가

--- src/test/java/cmmoon/tobyspringboot/HelloRepositoryTest.java
+++ src/test/java/cmmoon/tobyspringboot/HelloRepositoryTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 //@HelloBootTest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
 public class HelloRepositoryTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
